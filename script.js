7a1d039044e6ef912bc86ba1d3a909536c9457eb
cokingtins1 <106932061+cokingtins1@users.noreply.github.com>
2023-07-13 11:24:41 -0400

    updates from codespace

--- script.js
+++ script.js
@@ -8,25 +8,38 @@ const contactButton = document.getElementById('contact')
 
 const headerButtons = document.querySelectorAll('.headerButton')
 
-headerButtons.forEach(button =>{
-    button.addEventListener('click', () =>{
-        if(button.id === "home"){
+headerButtons.forEach(button => {
+
+    button.addEventListener('click', () => {
+        if (button.id === "home") {
             loadHome()
-        } else if(button.id === "menu"){
+        } else if (button.id === "menu") {
             loadMenu()
-        } else if(button.id === "contact"){
+        } else if (button.id === "contact") {
             loadContact()
         }
     })
+
 })
 
-function loadHome(){
-    console.log('home')
+function loadHome() {
+    createMenuItems()
 }
 
-function loadMenu(){
-    console.log('menu')
+function loadMenu() {
+    createMenuItems()
+    menuButton.classList.toggle('menu')
+    // populateMenu()
 }
+
+function loadContact() {
+
+    console.log('contact')
+    const fish = new MenuItem(itemArray[0], descArray[0], priceArray[0])
+    console.log(`On the menu tonight is ${fish.item} which is ${fish.desc} and reasonably priced at ${fish.price}`)
+}
+
+const items = []
 
 const itemArray = []
 itemArray[0] = "fish"
@@ -36,26 +49,24 @@ itemArray[2] = "steak"
 const descArray = []
 descArray[0] = "really good fish"
 descArray[1] = "the best pasta you ever had BETCH"
-descArray[3] = "cooked to perfection at medium rare"
-
-const priceArray = []
-priceArray[0] = "$69"
-priceArray[1] = "$420"
-priceArray[2] = "$9,000"
+descArray[2] = "cooked to perfection at medium rare"
 
 
 
+const calArray = []
+calArray[0] = "69"
+calArray[1] = "420"
+calArray[2] = "9,000"
 
-function loadContact(){
-    console.log('contact')
-    const fish = new MenuItem('fish','really good fish','$69')
-    console.log(`On the menu tonight is ${fish.item} which is ${fish.desc} and reasonably priced at ${fish.price}`)
-}
 
-class MenuItem{
-    constructor(item, desc, price){
-        this.item = item
-        this.desc = desc
-        this.price = price
+function createMenuItems() {
+    for (i = 0; i < itemArray.length; i++) {
+        const menuItems = {
+            item: itemArray[i],
+            desc: descArray[i],
+            calories: calArray[i]
+        }
+        items.push(menuItems) //creates array of menu item object
+        populateMenu()
     }
 }
